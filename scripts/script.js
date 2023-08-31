47957f26a2487c1e384cff6f7609e0a0dc2468e7
sachin <sachinm06@aol.com>
2023-09-01 02:58:48 +0530

    new changes

--- scripts/script.js
+++ scripts/script.js
@@ -9,7 +9,21 @@ $(document).ready(function(){
     })
 })
 
-function toggleNavbarBackground() {
-    var navbar = document.querySelector('.navbar');
-    navbar.classList.toggle('bg-white');
-}
+// function toggleNavbarBackground() {
+//     var navbar = document.querySelector('.navbar');
+//     navbar.classList.toggle('bg-white');
+// }
+
+// ------------search icon--------------
+
+const imageElement = document.getElementById('toggleImage');
+const inputBox = document.getElementById('inputBox');
+const imageSrcArray = ["./image/search.png", "./image/search_close.jpg"]; // Add more image URLs if needed
+let currentImageIndex = 0;
+
+imageElement.addEventListener('click', () => {
+    inputBox.classList.toggle('hidden');
+
+    currentImageIndex = (currentImageIndex + 1) % imageSrcArray.length;
+    imageElement.src = imageSrcArray[currentImageIndex];
+});
