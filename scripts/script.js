f57f27bd6753dc76cc7a9ae823f81e830f157922
sachin <sachinm06@aol.com>
2023-09-03 23:26:55 +0530

    new changes

--- scripts/script.js
+++ scripts/script.js
@@ -22,3 +22,14 @@ imageElement.addEventListener('click', () => {
     currentImageIndex = (currentImageIndex + 1) % imageSrcArray.length;
     imageElement.src = imageSrcArray[currentImageIndex];
 });
+
+// -------------voice search-------------
+
+function record() {
+    var recognition = new webkitSpeechRecognition();
+    recognition.lang = "en-GB";
+    recognition.onresult = function (event) {
+        document.getElementById('speechToText').value=event.results[0][0].transcript;    
+    }
+    recognition.start();
+}
